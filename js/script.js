6b370b99fbd33f65a0432612958a19fce2f34e99
Shreyas Dalvi <shreyasd525@gmail.com>
2020-09-23 11:10:33 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,65 @@
+$('.js-wp-1').waypoint(function (direction) {
+    $('.js-wp-1').addClass('animate__animated animate__fadeInLeft');
+}, {
+    offset: '50%'
+});
+
+$('.js-wp-2').waypoint(function (direction) {
+    $('.js-wp-2').addClass('animate__animated animate__fadeInRight');
+}, {
+    offset: '50%'
+});
+
+$('.js-wp-3').waypoint(function (direction) {
+    $('.js-wp-3').addClass('animate__animated animate__fadeIn');
+}, {
+    offset: '50%'
+});
+
+$('.js-wp-4').waypoint(function (direction) {
+    $('.js-wp-4').addClass('animate__animated animate__fadeInUp');
+}, {
+    offset: '50%'
+});
+
+$('.js-wp-5').waypoint(function (direction) {
+    $('.js-wp-5').addClass('animate__animated animate__fadeInDown');
+}, {
+    offset: '50%'
+});
+
+$('.js-wp-6').waypoint(function (direction) {
+    $('.js-wp-6').addClass('animate__animated animate__fadeIn');
+}, {
+    offset: '50%'
+});
+
+$('.js-wp-7').waypoint(function (direction) {
+    $('.js-wp-7').addClass('animate__animated animate__bounceIn');
+}, {
+    offset: '60%'
+});
+
+$('.js-wp-8').waypoint(function (direction) {
+    $('.js-wp-8').addClass('animate__animated animate__fadeIn');
+}, {
+    offset: '50%'
+});
+
+$('.js-wp-9').waypoint(function (direction) {
+    $('.js-wp-9').addClass('animate__animated animate__fadeInLeft');
+}, {
+    offset: '50%'
+});
+
+$('.js-wp-10').waypoint(function (direction) {
+    $('.js-wp-10').addClass('animate__animated animate__fadeInRight');
+}, {
+    offset: '50%'
+});
+
+$('.js-wp-1').waypoint(function (direction) {
+    $('.js-wp-1').addClass('animate__animated animate__fadeIn');
+}, {
+    offset: '50%'
+});
